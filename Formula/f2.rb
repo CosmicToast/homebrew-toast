aa5c19959cbf572ddf384326918e408ab2353d0a
Chloe Kudryavtsev <toast@toast.cafe>
2021-08-13 21:04:54 -0400

    f2: fix lint errors

--- Formula/f2.rb
+++ Formula/f2.rb
@@ -1,8 +1,8 @@
 class F2 < Formula
-  desc "F2 is a cross-platform command-line tool for batch renaming files and directories quickly and safely. Written in Go!"
+  desc "CLI tool for batch renaming files and directories."
   homepage "https://github.com/ayoisaiah/f2/"
-  head "https://github.com/ayoisaiah/f2.git"
   url "https://github.com/ayoisaiah/f2/archive/refs/tags/v1.7.1.tar.gz"
+  head "https://github.com/ayoisaiah/f2.git"
   sha256 "a59be93e6c5dcfc2b2536fd5b04d526d254cbe193ea9d34e645d8941034b2c1b"
   license "MIT"
 
